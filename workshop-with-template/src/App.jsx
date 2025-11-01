df9f3377f8f7f9a7e58b8c3e94784b960c64732a
IvanIvanovJS <sibsmartco@gmail.com>
2025-11-01 20:59:52 +0200

    refactored big futures into one jsx component

--- workshop-with-template/src/App.jsx
+++ workshop-with-template/src/App.jsx
@@ -3,7 +3,7 @@ import Contacts from "./components/Contacts"
 import Counter from "./components/Counter"
 import Footer from "./components/Footer"
 import BigFuturesDown from "./components/futures/BigFuturesDown"
-import BigFuturesTop from "./components/futures/BigFuturesTop"
+import BigFutures from "./components/futures/BigFutures"
 import SmallFutures from "./components/futures/SmallFutures"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
@@ -21,8 +21,22 @@ function App() {
             <Header />
             <Hero />
             <SmallFutures />
-            <BigFuturesTop />
-            <BigFuturesDown />
+            <BigFutures
+                title='Let’s discuss about you project'
+                imageUrl='assets/images/left-image.png'
+                content='Nullam sit amet purus libero. Etiam ullamcorper nisl ut augue
+                                blandit, at finibus leo efficitur. Nam gravida purus non sapien
+                                auctor, ut aliquam magna ullamcorper.'
+            />
+            <BigFutures
+                title='We can help you to grow your business'
+                imageUrl='assets/images/right-image.png'
+                content='Aenean pretium, ipsum et porttitor auctor, metus ipsum iaculis
+                                nisi, a bibendum lectus libero vitae urna. Sed id leo eu dolor
+                                luctus congue sed eget ipsum. Nunc nec luctus libero. Etiam quis
+                                dolor elit.'
+                isReversed
+            />
             <Parallax />
             <Testimonials />
             <Pricing />
