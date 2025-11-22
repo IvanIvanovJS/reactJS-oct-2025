69b117ccf6b4499650e8833154d57b1d887af3c9
IvanIvanovJS <sibsmartco@gmail.com>
2025-11-22 20:47:16 +0200

    feat(Register) add submit handler and input data

--- workshop-i-love-games/src/components/auth/login/Login.jsx
+++ workshop-i-love-games/src/components/auth/login/Login.jsx
@@ -5,10 +5,10 @@ export default function Login() {
             <form id="login">
                 <div className="container">
                     <h1>Login</h1>
-                    <label for="email">Email</label>
+                    <label htmlFor="email">Email</label>
                     <input type="email" id="email" name="email" placeholder="Your Email" />
 
-                    <label for="login-pass">Password</label>
+                    <label htmlFor="login-pass">Password</label>
                     <input type="password" id="login-password" name="password" placeholder="Password" />
                     <input type="submit" className="btn submit" value="Login" />
                 </div>
--- workshop-i-love-games/src/components/auth/register/Register.jsx
+++ workshop-i-love-games/src/components/auth/register/Register.jsx
@@ -1,19 +1,26 @@
 export default function Register() {
+
+    const submitHandler = (formData) => {
+        const { email, password, repassword } = Object.fromEntries(formData);
+        console.log(email, password, repassword);
+
+    }
+
     return (
         <section id="register-page" className="content auth">
-            <form id="register">
+            <form id="register" action={submitHandler}>
                 <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Register</h1>
 
-                    <label for="email">Email:</label>
+                    <label htmlFor="email">Email:</label>
                     <input type="email" id="email" name="email" placeholder="Your Email" />
 
-                    <label for="pass">Password:</label>
+                    <label htmlFor="pass">Password:</label>
                     <input type="password" name="password" id="register-password" placeholder="Password" />
 
-                    <label for="con-pass">Confirm Password:</label>
-                    <input type="password" name="confirm-password" id="confirm-password" placeholder="Repeat Password" />
+                    <label htmlFor="con-pass">Confirm Password:</label>
+                    <input type="password" name="repassword" id="confirm-password" placeholder="Repeat Password" />
 
                     <input className="btn submit" type="submit" value="Register" />
 
