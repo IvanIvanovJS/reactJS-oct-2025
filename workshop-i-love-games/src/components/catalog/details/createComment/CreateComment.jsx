29911241599cbbdeb3ede771b12958bba8bc95d6
IvanIvanovJS <sibsmartco@gmail.com>
2025-11-24 16:32:09 +0200

    feat(Comments) Create comment component controlled form implemented  and post comment data

---
+++ workshop-i-love-games/src/components/catalog/details/createComment/CreateComment.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+
+export default function CreateComment({
+    gameId,
+}) {
+    const [commentInfo, setCommentInfo] = useState({
+        gameId: gameId,
+        comment: '',
+    })
+
+
+
+    const onSubmitHandler = async () => {
+
+        try {
+            await fetch('http://localhost:3030/jsonstore/comments', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(commentInfo)
+            })
+        } catch (error) {
+            return alert(error.message)
+        }
+    }
+
+
+    const onChange = (e) => {
+        setCommentInfo(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    return (
+        <article className="create-comment">
+            <label>Add new comment:</label>
+            <form className="form" action={onSubmitHandler}>
+                <textarea name="comment" value={commentInfo.comment} onChange={onChange} placeholder="Comment......"></textarea>
+                <input className="btn submit" type="submit" value="Add Comment" />
+            </form>
+        </article>
+    )
+}
