cf9d574df95918ff89d367d2fdd4fd7ea152fd51
IvanIvanovJS <sibsmartco@gmail.com>
2025-11-24 10:18:22 +0200

    feat(Edit) Dynamic render data, controlled form handling and patch game data

--- workshop-i-love-games/src/components/catalog/edit/EditGame.jsx
+++ workshop-i-love-games/src/components/catalog/edit/EditGame.jsx
@@ -1,14 +1,25 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router"
+import { useNavigate, useParams } from "react-router"
+
+const initialValues = {
+    title: '',
+    genre: '',
+    players: '',
+    date: '',
+    imageUrl: '',
+    summary: ''
+}
 
 export default function EditGame() {
 
-    const { gameId } = useParams()
-    const [game, setGame] = useState({})
+    const { gameId } = useParams();
+    const [values, setValues] = useState(initialValues);
+    const navigate = useNavigate();
 
     useEffect(() => {
+        const controller = new AbortController()
+
         async function getGameById() {
-            const controller = new AbortController()
             try {
                 const res = await fetch(`http://localhost:3030/jsonstore/games/${gameId}`,
                     { signal: controller.signal }
@@ -16,50 +27,117 @@ export default function EditGame() {
 
                 const data = await res.json()
 
-                setGame(data)
+
+                setValues(data)
             } catch (err) {
                 alert(err.message)
             }
-
-            return () => controller.abort()
         }
         getGameById()
+
+        return () => controller.abort()
     }, [gameId])
+
+
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+
+
+        try {
+            await fetch(`http://localhost:3030/jsonstore/games/${gameId}`, {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(values),
+
+            })
+        } catch (error) {
+            return alert(error.message)
+        }
+        navigate(`/games`)
+    }
+
+
+
+
+    const onChangeHandler = (e) => {
+        setValues(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }))
+    }
+
     return (
         <section id="edit-page">
-            <form id="add-new-game">
+            <form id="add-new-game" onSubmit={onSubmit}>
                 <div className="container">
 
                     <h1>Edit Game</h1>
 
                     <div className="form-group-half">
                         <label htmlFor="gameName">Game Name:</label>
-                        <input type="text" id="gameName" name="gameName" placeholder="Enter game title..." />
+                        <input
+                            type="text"
+                            id="gameName"
+                            name="title"
+                            onChange={onChangeHandler}
+                            value={values.title}
+                            placeholder="Enter game title..." />
                     </div>
 
                     <div className="form-group-half">
                         <label htmlFor="genre">Genre:</label>
-                        <input type="text" id="genre" name="genre" placeholder="Enter game genre..." />
+                        <input
+                            type="text"
+                            id="genre"
+                            name="genre"
+                            onChange={onChangeHandler}
+                            value={values.genre}
+                            placeholder="Enter game genre..." />
                     </div>
 
                     <div className="form-group-half">
                         <label htmlFor="activePlayers">Active Players:</label>
-                        <input type="number" id="activePlayers" name="activePlayers" min="0" placeholder="0" />
+                        <input
+                            type="number"
+                            id="activePlayers"
+                            name="players"
+                            min="0"
+                            onChange={onChangeHandler}
+                            value={values.players}
+                            placeholder="0" />
                     </div>
 
                     <div className="form-group-half">
                         <label htmlFor="releaseDate">Release Date:</label>
-                        <input type="date" id="releaseDate" name="releaseDate" />
+                        <input
+                            type="date"
+                            id="releaseDate"
+                            onChange={onChangeHandler}
+                            value={values.date}
+                            name="date" />
                     </div>
 
                     <div className="form-group-full">
                         <label htmlFor="imageUrl">Image URL:</label>
-                        <input type="text" id="imageUrl" name="imageUrl" placeholder="Enter image URL..." />
+                        <input
+                            type="text"
+                            id="imageUrl"
+                            name="imageUrl"
+                            onChange={onChangeHandler}
+                            value={values.imageUrl}
+                            placeholder="Enter image URL..." />
                     </div>
 
                     <div className="form-group-full">
                         <label htmlFor="summary">Summary:</label>
-                        <textarea name="summary" id="summary" rows="5"
+                        <textarea name="summary"
+                            id="summary"
+                            rows="5"
+                            onChange={onChangeHandler}
+                            value={values.summary}
                             placeholder="Write a brief summary..."></textarea>
                     </div>
 
